dfe4307efd0e7c1d54b9aa92dc3ca37e9751a618
rojinyilmaz <70367334+rojinyilmaz@users.noreply.github.com>
2021-02-17 14:47:20 +0100

    Added props

--- ewallet/src/components/Top.vue
+++ ewallet/src/components/Top.vue
@@ -1,16 +1,26 @@
 <template>
-  <header>
-      top
-  </header>
+ <div class="top">
+<h1>{{headline}}</h1>
+<p>{{description}}</p>
+
+</div>
 </template>
 
 <script>
 export default {
-    name: "Top"
+    name: "Top",
+    props:{
+      headline: String,
+      description: String
+    }
 
 }
 </script>
 
 <style>
+
+p{
+  text-align: center;
+}
 
 </style>
--- ewallet/src/views/AddCard.vue
+++ ewallet/src/views/AddCard.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="addcard">
+    <top headline="ADD A NEW BANK CARD" description="NEW CARD"/>
     <Card />
     <CardForm />
   </div>
 </template>
 
 <script>
+import Top from '@/components/Top.vue'
 import Card from "@/components/Card"
 import CardForm from "@/components/CardForm"
+
 
 export default {
   name: 'AddCard',
   components: {
+   Top,
    Card,
    CardForm
   }
